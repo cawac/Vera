80f12b570ffe44b55c56e2e642ecfd90ddc5aa1c
cawac <cawapl33@gmail.com>
2021-11-08 18:52:09 +0300

    start

---
+++ lab 2/Clothes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alex on 08/11/2021.
+//
+
+#include "Clothes.h"
---
+++ lab 2/Clothes.h
@@ -0,0 +1,17 @@
+//
+// Created by alex on 08/11/2021.
+//
+
+#ifndef LAB_2_CLOTHES_H
+#define LAB_2_CLOTHES_H
+
+
+class Clothes {
+private:
+
+protected:
+public:
+};
+
+
+#endif //LAB_2_CLOTHES_H
